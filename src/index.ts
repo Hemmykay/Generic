9f1d0ec004aa3fd4b76cd36deff1fb44a7a6fde9
Hemmykay <kehindeemmanuel406@gmail.com>
2024-09-28 20:21:08 +0100

    Interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,31 @@
-type User = {
-    name: string,
-    publicAddress: string,
-    email: string
-}
+/* We use interfaces to define the interface or shape of an object
 
-const list = document.querySelector<HTMLUListElement>('#list')
-const form = <HTMLFormElement | null> document.getElementById('new-task-form')
-const input1 = document.querySelector<HTMLInputElement>('#new-name')
-const input2 = document.querySelector<HTMLInputElement>('#new-email')
-const input3 = document.querySelector<HTMLInputElement>('#new-public-address')
-const users: User[] = loadTasks()
-users.forEach(addListItem)
-form?.addEventListener('submit', e => {
-    e.preventDefault()
-
-    if (input1?.value == '' || input1?.value == null || input2?.value == '' || input2?.value == null || input3?.value == '' || input3?.value == null) return
-
-    const newUser: User = {
-        name: input1.value,
-        publicAddress: input2.value,
-        email: input3.value
+    Let's say we want to represent a calendar concept
+    Remember, We have different calender implementations
+    - Google
+    - iCal
+    - Outlook
+    All this calenders should have some common properties an methods
+    How we record event on google can be different. 
+    Therefore, we should make the methods abstract,
+    which will require we make the base class an abstract
+    ----------------------------------------------------------------
+    abstract class Calendar {
+        constructor(public name: string){}
+    
+        abstract addEvent() : void;
+        abstract removeEvent(): void;
     }
 
-    users.push(newUser)
-    saveTasks()
+    Now, we can create concrete implementations like Google Calendar and so on
+    This can be done better using interfaces.
 
-    addListItem(newUser);
-    input1.value = ''
-    input2.value = ''
-    input3.value = ''
-})
-
-function addListItem(user: User) {
-    const item = document.createElement('li')
-    const label = document.createElement('label')
-    saveTasks()
-    label.append(user.name, ' ', user.email, ' ', user.publicAddress)
-    item.append(label)
-    list?.append(item)
-}
-
-function saveTasks() {
-    localStorage.setItem('USERS', JSON.stringify(users))
-}
-function loadTasks(): User[] {
-    const userJSON = localStorage.getItem('USERS')
-    if (userJSON == null) return []
-    return JSON.parse(userJSON)
+    If we have any logic, algorithm or some method with few lines of code,
+    Then Interface won't be suitable
+    Interfaces cannot have methods implementation.
+*/
+interface Calendar {
+    name : string;
+    addEvent() : void;
+    removeEvent() : void;
 }
