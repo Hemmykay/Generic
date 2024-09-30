1c0a7580865707004fc080e5b9c111ace9eed8d3
Hemmykay <kehindeemmanuel406@gmail.com>
2024-09-30 22:43:55 +0100

    Understanding the problem

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+/*
+    Understanding the problem
+    --------------------------------
+    Let's say you want to represent  KeyValuePair: so we define a class:
+    --------------------------------
+    class KeyValuePair {
+    constructor(public key: number, public value: string){}
+    }
+
+    Now we can crate an object with:
+
+    let pair = new KeyValuePair('1', '2');
+    --------------------------------
+    Now let's say in our app we wanted to use a string value for a key, with our current implemetation, we can't do that.
+*/
 class KeyValuePair<TKey, TValue> {
     constructor(public key: TKey, public value: TValue){}
 }
