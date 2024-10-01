1fe4d30cecaf1fb5b72cd65cf083d5e94c214fa0
Hemmykay <kehindeemmanuel406@gmail.com>
2024-10-01 23:48:31 +0100

    Lay-man solution

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,28 @@
 
     let pair = new KeyValuePair('1', '2');
     --------------------------------
-    Now let's say in our app we wanted to use a string value for a key, with our current implemetation, we can't do that.
+    Now let's say in our app we wanted to use a string value for a key, with our current implementation, we can't do that.
+
+    We have two solutions:
+    1 - We can use "any":
+    `
+    class KeyValuePair {
+    constructor(public key: any, public value: string){}
+    }
+    `
+    But with 'any' we loose type safety
+
+    2 - We can duplicate the class:
+    `
+    class KeyValuePair {
+    constructor(public key: number, public value: string){}
+    }
+
+    class KeyValuePair {
+    constructor(public key: string, public value: string){}
+    }
+    `
+    But this is not a good solution as it is redundant. We have to keep duplicating the class for every typ we need.
 */
 class KeyValuePair<TKey, TValue> {
     constructor(public key: TKey, public value: TValue){}
